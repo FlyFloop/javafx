92809bb3a306205cc627f90c653c177675ad6da9
Alper Yorgun <alperyorgun2@gmail.com>
2021-06-21 15:51:37 +0300

    can you not see that

---
+++ AlperBank/src/module-info.java
@@ -0,0 +1,11 @@
+module AlperBank {
+    requires javafx.web;
+    requires javafx.base;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.controls;
+    requires javafx.swing;
+    requires java.sql;
+    opens sample;
+}
